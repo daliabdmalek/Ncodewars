090570aaf9ff6bcd57d9f12f5ce27cc016569deb
tamarushin <tamarushin@gmail.com>
2018-03-17 13:39:34 -0700

    completed my-head-wrong-end

--- solution.js
+++ solution.js
@@ -1,5 +1,7 @@
 'use strict';
 
-var replaceDots = function (str) {
-  return str.replace(/\./g, '-');
+function fixTheMeerkat(arr) {
+  return arr.reverse();
 }
+
+fixTheMeerkat(["tail", "body", "head"]);
